a77e4573b7da1da8b952e9aebe203605cfc87596
Brian B <info@missionmedinstitute.com>
2026-09-21 10:33:15 -0400

    add private RankListIQ canary page setup

---
+++ ranklistiq/canary/configure-private-canary.php
@@ -0,0 +1,29 @@
+<?php
+/** Configure the existing withdrawn canary page as an admin-only private page. */
+
+declare(strict_types=1);
+
+$content = <<<'HTML'
+<iframe
+  title="MissionMed RankListIQ protected canary"
+  src="https://missionmedinstitute.com/rlq-dualmode-0921a-canary.php?mode=application"
+  style="display:block;width:100%;min-height:1000px;border:0"
+  loading="eager"
+></iframe>
+HTML;
+
+$result = wp_update_post(
+    [
+        'ID' => 9174,
+        'post_status' => 'private',
+        'post_content' => $content,
+    ],
+    true
+);
+
+if (is_wp_error($result)) {
+    WP_CLI::error($result->get_error_message());
+}
+
+WP_CLI::success('Configured protected RankListIQ canary post ' . (string) $result);
+
